1803ab492ab129de210235a258b132adce5bf825
Ashfinn <obidur.shawal@gmail.com>
2023-03-19 22:59:33 +0600

    modified:   1D and loop/9.py

--- 1D and loop/9.py
+++ 1D and loop/9.py
@@ -1,2 +1,13 @@
-n = input('n = ? : ')
-i for i in range(1, int(n))
+'''
+Finding the value of sin(x) with the following algorithm:
+sin(x) = x^1/1! - x^3/3! + x^5/5! - x^7/7! + ..... + Infinity
+It can be simplified to:
+
+sin(x) = Σ ((-1)^n * x^(2 * n + 1) / (2 * n + 1)!)
+where x => 0 to Infinity, n = accuracy (higher -> more precise)
+'''
+from math import factorial
+n = int(input('n = ? : '))
+x = int(input('x = ? : '))
+for i in range(1,n):
+    print(sum((-1)**n * x**(2 * n + 1) /factorial(2* n + 1)))
